5939cc92e92f66cc415255e1025dcc5c9eccbd7f
William Dimaculangan <william_dimaculangan@dlsu.edu.ph>
2022-05-07 16:59:57 +0800

    file open

--- main.c
+++ main.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
-{
-  printf("Hello world");
+int main() {
+
+  char fileName[100];
+  FILE *fp; //file pointer
+
+  printf("Input the name of the input text file: ");
+  scanf("%s", fileName);
+  fp = fopen(fileName, "r");
+
+  if (fp != NULL) {
+    printf("File found!");
+    while(!feof(fp)) {
+      
+    }
+  } else {
+    printf("%s not found.", fileName);
+  }
+  
+  fclose(fp);
   return 0;
 }
