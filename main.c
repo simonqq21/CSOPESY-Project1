397e084b5df673609cab8a84e9e8ea341ce04b75
Simon Que <simon_justin_que@dlsu.edu.ph>
2022-05-07 19:43:17 +0800

    read initial line

--- main.c
+++ main.c
@@ -5,6 +5,7 @@ int main() {
 
   char fileName[100];
   FILE *fp; //file pointer
+  int nSchedAlgo, y, z;
 
   printf("Input the name of the input text file: ");
   scanf("%s", fileName);
@@ -12,9 +13,32 @@ int main() {
 
   if (fp != NULL) {
     printf("File found!");
-    while(!feof(fp)) {
-
+    //get number to see if scheduling algo
+    fscanf(fp, "%d %d %d", &nSchedAlgo, &y, &z);
+    printf("Numbers from file: %d, %d, %d\n", nSchedAlgo, y, z);
+    switch (nSchedAlgo) {
+      //if FCFS
+      case 0:
+        printf("FCFS algorithm will be performed");
+        break;
+      //if SJF
+      case 1:
+        printf("SJF algorithm will be performed");
+        break;
+      //if SRTF
+      case 2:
+        printf("SRTF algorithm will be performed");
+        break;
+      //if RR
+      case 3:
+        printf("RR algorithm will be performed");
+        break;
     }
+
+    //check if valid input
+    // while(!feof(fp)) {
+    //
+    // }
     fclose(fp);
   } else {
     printf("%s not found.", fileName);
--- main2.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int int main() {
-  char fileName[100];
-  
-  return 0;
-}
