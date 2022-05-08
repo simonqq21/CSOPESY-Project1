91a1797f53b939367ae88aeed7320fe9cb857d81
Simon Que <simon_justin_que@dlsu.edu.ph>
2022-05-08 21:31:38 +0800

    started adding the structs

--- main.c
+++ main.c
@@ -1,18 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+/* Each job has a pid, burst time, and arrival time. Each job also has a
+dynamically allocated array of timeframes. */
+// Each timeframe has a start time and an end time.
+// Sort the jobs based on arrival time in ascending order
+
+typedef struct Timeframe {
+  int start;
+  int end;
+  struct Timeframe * next;
+} Timeframe_t;
+
+typedef struct Process {
+  int pid;
+  int arrival;
+  int burst;
+  Timeframe_t * timeframe
+} Process_t;
+
+
 int main() {
 
   char fileName[100];
   FILE *fp; //file pointer
-  int nSchedAlgo, y, z;
+  int nSchedAlgo, y, z; // variables for first line
+  Process_t processes[100];
 
   printf("Input the name of the input text file: ");
   scanf("%s", fileName);
   fp = fopen(fileName, "r");
 
   if (fp != NULL) {
-    printf("File found!");
     //get number to see if scheduling algo
     fscanf(fp, "%d %d %d", &nSchedAlgo, &y, &z);
     printf("Numbers from file: %d, %d, %d\n", nSchedAlgo, y, z);
@@ -41,7 +61,7 @@ int main() {
     // }
     fclose(fp);
   } else {
-    printf("%s not found.", fileName);
+    printf("%s not found.\n", fileName);
   }
 
   return 0;
