9e96249c0de83c18d782568ef371dc74d14fe7cf
Simon Que <simon_justin_que@dlsu.edu.ph>
2022-05-11 11:15:27 +0800

    x

--- types.c
+++ types.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include "types.h"
 
+Process_t* createProcess(int pid, int arrival, int burst) {
+  Process_t p = (Process_t*) malloc (sizeof(Process_t*));
+  p -> pid = pid;
+  p -> 
+};
+
 ProcessQueue* createProcessQueue(void) {
   ProcessQueue * q = (ProcessQueue*) malloc (sizeof(ProcessQueue));
   q -> head = q -> tail = NULL;
