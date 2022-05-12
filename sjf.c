1fa66b86dd2f56c2fac7ec2a576bf3a1e62cd7d3
William Dimaculangan <william_dimaculangan@dlsu.edu.ph>
2022-05-12 16:12:10 +0800

    initial code for sjf

--- sjf.c
+++ sjf.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "types.h"
 #include "sjf.h"
 
@@ -48,75 +49,124 @@
 // }
 
 // /* This function inserts to the waiting list when time is */
-void insertToWaitList (Process_t *arrivedProcess, Process_t *waitingListHead) {
-    Process_t *newProcess = malloc(sizeof(Process_t));
+// void insertToWaitList (Process_t *arrivedProcess, Process_t *waitingListHead) {
+//     Process_t *newProcess = malloc(sizeof(Process_t));
 
-    if(newProcess == NULL) {
-        return 0; //0 for false
-    } else {
-        *newProcess = *arrivedProcess; //copy the contents
+//     if(newProcess == NULL) {
+//         printf("Memory not allocated.\n");
+//     } else {
+//         *newProcess = *arrivedProcess; //copy the contents
 
-        //If list is empty
-        if(waitingListHead == NULL) {
-            waitingListHead = newProcess;
-            waitingListHead->next = NULL;
-        } else {
+//         //If list is empty
+//         if(waitingListHead == NULL) {
+//             waitingListHead = newProcess;
+//             waitingListHead->next = NULL;
+//         } else {
 
-        }
-        //If there is existing tail connect 
-        if (q->tail != NULL) {
-            q->tail->next = newProcess; 
-        }
-        q->tail = newProcess; //new tail 
+//         }
+//         //If there is existing tail connect 
+//         if (q->tail != NULL) {
+//             q->tail->next = newProcess; 
+//         }
+//         q->tail = newProcess; //new tail 
 
-        //If queue is empty
-        if (q->head == NULL) {
-            q->head = newProcess; //point to new process
-        }
-        return 1; //return true
-    }
-}
+//         //If queue is empty
+//         if (q->head == NULL) {
+//             q->head = newProcess; //point to new process
+//         }
+//         return 1; //return true
+//     }
+// }
 
-void searchArrivedProcess (int currTime, Process_t *processList, Process_t *waitingList) {
-    //Search for the processes <= current time and insert them to the waiting list, deleting inserted ones from the process list
+/*This function deletes a specific process from the process list */
+int deleteProcess(int pid, Process_t *processList, int numProcesses) {
     Process_t *tmp = processList;
+    Process_t *nextNode;
+    int found = 0;
+
+    //if head will be deleted
+    if(tmp->pid == pid) {
+        processList = tmp->next; //new head will be the head's next element
+        free(tmp);
+        numProcesses = numProcesses - 1;
+    } else {
+        while (tmp != NULL && !found) {
+            nextNode = tmp->next;
+            if(nextNode->pid == pid) {
+                tmp->next = nextNode->next;
+                free(nextNode);
+                numProcesses = numProcesses - 1;
+                found = 1;
+            }
+            tmp = tmp->next;
+        }
+    }
+    
+    return numProcesses;
+}
+/* This function searches the shortest burst process that has already arrived */
+Process_t *searchArrivedShortestProcess (int currTime, Process_t *processList) {
+    Process_t *tmp = processList;
+    Process_t *minProcess = NULL;
+    int minBurst;
+
+    minBurst = tmp->burst;
     //Traverse through the process list
     while (tmp != NULL) {
-        if (tmp->arrival <= currTime) {
-            insertToWaitList(tmp, waitingList);
-            //sortwaitinglist to shortest burst time
-            //then just 
-            //deleteNodeFromList(processList, tmp->pid);
+         //Search for the processes <= current time and insert them to the waiting list, deleting inserted ones from the process list
+        if (tmp->arrival <= currTime && tmp->burst < minBurst) {
+            //Allocate memory to be able to copy files if not done yet
+            if(minProcess == NULL) {
+                minProcess = malloc(sizeof(Process_t));
+            }
+            *minProcess = *tmp;
+            minBurst = tmp->burst;
         }
         tmp = tmp->next;
     }
+    return minProcess;
 }
 
 // /* For checking purposes */
-void printList(Process_t *head) {
-    Process_t *tmp = head;
-    while(tmp != NULL) {
-        printf("Pid: %d, AT: %d, BT: %d\n", tmp->pid, tmp->arrival, tmp->burst);
-        tmp = tmp->next; 
-    }
-}
+// void printList(Process_t *head) {
+//     Process_t *tmp = head;
+//     while(tmp != NULL) {
+//         printf("[Delete me] Pid: %d, AT: %d, BT: %d\n", tmp->pid, tmp->arrival, tmp->burst);
+//         tmp = tmp->next; 
+//     }
+// }
 
 /* Main function that performs SJF */
 void sjf(Process_t *processList, int numProcesses) {
-    printf("SJF ALGO\n");
     int currTime = 0;
-    Process_t *waitingListHead = malloc(sizeof(Process_t));
-    Process_t *executedProcessHead = malloc(sizeof(Process_t));
-    printf("Num of processes: %d", numProcesses);
-    while(numProcesses > 0) {
-        searchArrivedProcess(currTime, processList);
-        // currProcess = searchShortestBurst(waitingList);
-        // currProcess->timeframes->start = currTime;
-        // currProcess->timeframes->end = currProcess->timeframes->start + currProcess->burst;
-        // currTime = currProcess->timeframes->end;
-        // insertCompleteJobs(currTime);
-        // numProcesses = numProcesses - 1; 
-    }
+    //Process_t *waitingList = malloc(sizeof(Process_t)); //head of waiting list
+    Process_t *executedProcessHead = malloc(sizeof(Process_t)); //head of executed
+    Process_t *temp;
+    printf("[Delete me] SJF ALGO will be performed\n");
+    printf("[Delete me] Num of processes: %d\n", numProcesses);
 
+    if(executedProcessHead == NULL) {
+        printf("Memory is not allocated.\n");
+    } else {
+        while(numProcesses > 0) {
+            temp = searchArrivedShortestProcess(currTime, processList);
+            if(temp != NULL) {
+                printf("Minprocess: %d", temp->burst);
+                numProcesses = deleteProcess(temp->pid, processList, numProcesses);
+                printProcesses(processList);
+                //insert to executedProcess (where calculation of time frame is done)
+                //delete the process from the process list, using temp as reference or temp->pid
+            } else {
+                printf("[Delete me] No process has arrived yet at time %d.\n", currTime);
+            }
+            currTime++;
+            // currProcess = searchShortestBurst(waitingList);
+            // currProcess->timeframes->start = currTime;
+            // currProcess->timeframes->end = currProcess->timeframes->start + currProcess->burst;
+            // currTime = currProcess->timeframes->end;
+            // insertCompleteJobs(currTime);
+            // numProcesses = numProcesses - 1; 
+        }
+    }
     
 }
--- sjf.h
+++ sjf.h
@@ -1,1 +1,3 @@
+int deleteProcess(int pid, Process_t *processList, int numProcesses);
+Process_t *searchArrivedShortestProcess (int currTime, Process_t *processList);
 extern void sjf(Process_t *processList, int numProcesses);
